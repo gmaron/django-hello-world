7d771cb764de301ea184367078ac017389b25e6b
gmaron <gastonmaron@gmail.com>
2020-05-24 16:20:12 -0300

    Delete database config due to docker build and run
    It's to demostrate that the image runs.
    More functionality will be added soon.

--- hello_world/environment/development.py
+++ hello_world/environment/development.py
@@ -6,13 +6,5 @@ import os
 os.environ['SECRET_KEY'] = 'secret_key'
 os.environ['ALLOWED_HOST_DOMAIN'] = 'localhost:8000'
 
-# DATABASE ENVIRONMENT VARS
-os.environ['DB_HOST'] = 'localhost'
-os.environ['DB_PORT'] = '3306'
-os.environ['DB_USER'] = 'db_user'
-os.environ['DB_PASS'] = 'db_pass'
-os.environ['DB_NAME'] = 'db_name'
-os.environ['DB_ENGINE'] = 'django.db.backends.mysql' # for mysql engine. 
-
 # OTHER APP ENVIRONMENT VARS
 # ...
